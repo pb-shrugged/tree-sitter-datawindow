424e7251f6803656a1ad1148c85c2d45b1f158f0
devjosecagnini <jose.cagnini@proton.me>
2026-04-16 18:33:56 -0300

    fix: c bindings

---
+++ bindings/c/tree_sitter/tree-sitter-datawindow-expression.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_DATAWINDOW_EXPRESSION_H_
+#define TREE_SITTER_DATAWINDOW_EXPRESSION_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_datawindow_expression(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_DATAWINDOW_EXPRESSION_H_
---
+++ bindings/c/tree_sitter/tree-sitter-datawindow-syntax.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_DATAWINDOW_SYNTAX_H_
+#define TREE_SITTER_DATAWINDOW_SYNTAX_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_datawindow_syntax(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_DATAWINDOW_SYNTAX_H_
